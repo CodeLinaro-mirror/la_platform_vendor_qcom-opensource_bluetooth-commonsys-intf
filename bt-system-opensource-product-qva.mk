839b171e44c0d596475888a7511fdfad7da6009c
Yanfei Zhou <quic_zyanfei@quicinc.com>
2023-09-05 12:27:39 +0530

    Set property for dual BT in Android framework (Part 2)
    Set the following properties to true when Dual BT is enabled, and to
    false when it is disabled.
    - "persist.bluetooth.dual_bt"
    - "persist.bluetooth.dual_adapter_mode"
    - "bluetooth.profile.hfp.ag.enabled"
    - "bluetooth.profile.a2dp.source.enabled"
    - "bluetooth.profile.avrcp.target.enabled"
    
    This change is to amend Change:
    I36f3ba289a5ffe7a359e2420a7c89a12644c7641
    
    Change-Id: I76201444feec3bec8fff3ff7d7f11a20dad0ca64

--- bt-system-opensource-product-qva.mk
+++ bt-system-opensource-product-qva.mk
@@ -14,10 +14,22 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 ifeq ($(BOARD_HAVE_DUAL_BLUETOOTH), true)
 PRODUCT_PRODUCT_PROPERTIES += \
-    persist.bluetooth.dual_bt=false \
+    persist.bluetooth.dual_bt=true \
     persist.bluetooth.dual_adapter_mode=true
 
 PRODUCT_PRODUCT_PROPERTIES += \
+    bluetooth.profile.hfp.ag.enabled=true \
     bluetooth.profile.a2dp.source.enabled=true \
     bluetooth.profile.avrcp.target.enabled=true
+else
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.bluetooth.dual_bt=false \
+    persist.bluetooth.dual_adapter_mode=false
+
+ifeq ($(TARGET_BOARD_TYPE),auto)
+PRODUCT_PRODUCT_PROPERTIES += \
+    bluetooth.profile.hfp.ag.enabled=false \
+    bluetooth.profile.a2dp.source.enabled=false \
+    bluetooth.profile.avrcp.target.enabled=false
+endif
 endif
