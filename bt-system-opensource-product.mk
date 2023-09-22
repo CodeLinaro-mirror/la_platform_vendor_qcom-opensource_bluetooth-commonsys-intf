9928529beb13e4dc804a74e8f7a60755364a411f
Rahul Samana <quic_samana@quicinc.com>
2023-09-22 14:59:51 +0530

    BT: commonsys-intf-bluetooth : LW.UM.3.0 Bringup 1.Added PRODUCT_SOONG_NAMESPACES to QC BT Stack 2.Updating bdroid_buildcfg.h configuration according to LW device for Successful Pairing and Account Sync
    Change-Id: Ifb893b1e01d4206a93f5413eaf56d5a2e87e7752

--- bt-system-opensource-product.mk
+++ bt-system-opensource-product.mk
@@ -1,5 +1,5 @@
 #ANT
-ifeq ($(TARGET_FWK_SUPPORTS_FULL_VALUEADDS), true)
+ifeq ($(TARGET_USE_WEAR_QC_BT_STACK), true)
 ifeq ($(BOARD_ANT_WIRELESS_DEVICE), "vfs-prerelease")
 PRODUCT_PACKAGES += AntHalService
 PRODUCT_PACKAGES += libantradio
@@ -10,7 +10,7 @@ PRODUCT_PACKAGES += AntHalService-Soong
 PRODUCT_PACKAGES += com.dsi.ant@1.0
 endif
 endif
-endif #TARGET_FWK_SUPPORTS_FULL_VALUEADDS
+endif #TARGET_USE_WEAR_QC_BT_STACK
 
 #BT
 ifeq ($(BOARD_HAVE_BLUETOOTH_QCOM),true)
@@ -27,10 +27,16 @@ PRODUCT_PRODUCT_PROPERTIES += \
     bluetooth.profile.hfp.hf.enabled=true
 endif #TARGET_BOARD_TYPE
 
-ifeq ($(TARGET_FWK_SUPPORTS_FULL_VALUEADDS), true)
-ifneq ($(TARGET_BOARD_TYPE),auto)
+ifeq ($(TARGET_USE_WEAR_QC_BT_STACK), true)
 TARGET_USE_QTI_BT_STACK := true
+
+ifeq ($(BOARD_OPENSOURCE_DIR),)
+    BOARD_OPENSOURCE_DIR := vendor/qcom/opensource
 endif
+
+SOONG_CONFIG_NAMESPACES += aosp_vs_qva
+SOONG_CONFIG_aosp_vs_qva += aosp_or_qva
+SOONG_CONFIG_aosp_vs_qva_aosp_or_qva := qva
 
 ifeq ($(TARGET_USE_QTI_BT_STACK),true)
 # BT Related Libs
@@ -38,16 +44,11 @@ PRODUCT_PACKAGES += libbluetooth_qti
 PRODUCT_PACKAGES += libbluetooth_qti_jni
 PRODUCT_PACKAGES += bt_logger
 PRODUCT_PACKAGES += libbt-logClient
-PRODUCT_PACKAGES += libbtconfigstore
-PRODUCT_PACKAGES += vendor.qti.hardware.btconfigstore@1.0
-PRODUCT_PACKAGES += vendor.qti.hardware.btconfigstore@2.0
+#PRODUCT_PACKAGES += libbtconfigstore
+#PRODUCT_PACKAGES += vendor.qti.hardware.btconfigstore@1.0
+#PRODUCT_PACKAGES += vendor.qti.hardware.btconfigstore@2.0
 PRODUCT_PACKAGES += com.qualcomm.qti.bluetooth_audio@1.0
 PRODUCT_PACKAGES += vendor.qti.hardware.bluetooth_audio@2.0
-
-
-ifeq ($(BOARD_OPENSOURCE_DIR),)
-    BOARD_OPENSOURCE_DIR := vendor/qcom/opensource
-endif
 
 ifeq ($(TARGET_USE_BT_DUN),true)
 PRODUCT_PACKAGES += vendor.qti.hardware.bluetooth_dun-V1.0-java
@@ -74,16 +75,36 @@ ifneq ($(TARGET_HAS_LOW_RAM), true)
 PRODUCT_PACKAGES_DEBUG += BTTestApp
 endif #TARGET_HAS_LOW_RAM
 
+ifneq ($(TARGET_PRODUCT),sdm429w)
+#adv audio
+SOONG_CONFIG_NAMESPACES += bredr_vs_btadva
+SOONG_CONFIG_bredr_vs_btadva += bredr_or_btadva
+SOONG_CONFIG_NAMESPACES += fwk_bredr_vs_btadva
+SOONG_CONFIG_fwk_bredr_vs_btadva += fwk_bredr_or_btadva
+
+ifneq "$(wildcard vendor/qcom/proprietary/commonsys/bt/bt_adv_audio)" ""
+    $(warning bt_adv_audio dir is present)
+    SOONG_CONFIG_bredr_vs_btadva_bredr_or_btadva := btadva
+    SOONG_CONFIG_fwk_bredr_vs_btadva_fwk_bredr_or_btadva := btadva
 else
-#PRODUCT_SOONG_NAMESPACES += packages/modules/Bluetooth/android/app
-#PRODUCT_PACKAGE_OVERLAYS += $BOARD_OPENSOURCE_DIR/commonsys-intf/bluetooth/overlay/generic
-#BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $BOARD_OPENSOURCE_DIR/commonsys-intf/bluetooth/build/generic/config
+    $(warning bt_adv_audio dir is not present)
+    SOONG_CONFIG_bredr_vs_btadva_bredr_or_btadva := bredr
+    SOONG_CONFIG_fwk_bredr_vs_btadva_fwk_bredr_or_btadva := bredr
+endif #ifneq "$(wildcard vendor/qcom/proprietary/commonsys/bt/bt_adv_audio)" ""
+endif
+
+else
+PRODUCT_SOONG_NAMESPACES += packages/modules/Bluetooth/android/app
+PRODUCT_PACKAGE_OVERLAYS += $BOARD_OPENSOURCE_DIR/commonsys-intf/bluetooth/overlay/generic
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $BOARD_OPENSOURCE_DIR/commonsys-intf/bluetooth/build/generic/config
 endif #TARGET_USE_QTI_BT_STACK
 
 else
+PRODUCT_SOONG_NAMESPACES += packages/modules/Bluetooth/android/app
 #PRODUCT_PACKAGE_OVERLAYS += $BOARD_OPENSOURCE_DIR/commonsys-intf/bluetooth/overlay/generic
 #BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $BOARD_OPENSOURCE_DIR/commonsys-intf/bluetooth/build/generic/config
-endif #TARGET_FWK_SUPPORTS_FULL_VALUEADDS
+SOONG_CONFIG_aosp_vs_qva_aosp_or_qva := aosp
+endif #TARGET_USE_WEAR_QC_BT_STACK
 
 endif #BOARD_HAVE_BLUETOOTH_QCOM
 
--- build/qva/config/bdroid_buildcfg.h
+++ build/qva/config/bdroid_buildcfg.h
@@ -20,15 +20,44 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 #define BTM_DEF_LOCAL_NAME   "QCOM-BTD"
-// Disables read remote device feature
-#define MAX_ACL_CONNECTIONS   16
-#define MAX_L2CAP_CHANNELS    64
-#define BLE_VND_INCLUDED   TRUE
-#define GATT_MAX_PHY_CHANNEL  10
-// skips conn update at conn completion
-#define BT_CLEAN_TURN_ON_DISABLED 1
+#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
+#define BTA_SKIP_BLE_READ_REMOTE_FEAT TRUE
+#define BLE_PERIPHERAL_MODE_SUPPORT   TRUE
+#define BLE_PERIPHERAL_DISPLAYONLY    TRUE
+#define BLE_DELAY_REQUEST_ENC         TRUE
+#define BLE_LOCAL_PRIVACY_ENABLED     FALSE
+#define BLE_VND_INCLUDED              TRUE
+#define BLE_DYNAMIC_LATENCY_ENABLED   FALSE
 
-#define AVDT_NUM_SEPS 35
+#define MAX_L2CAP_CHANNELS    14
+// skips conn update at conn completion
+#define BTA_BLE_SKIP_CONN_UPD  FALSE
+
+#define BTA_DM_COD {0x00, BTM_COD_MAJOR_WEARABLE, BTM_COD_MINOR_WRIST_WATCH}
+
+#define BTA_AR_INCLUDED               TRUE
+#define A2D_INCLUDED                  TRUE
+#define AVDT_INCLUDED                 TRUE
+#define AVCT_INCLUDED                 TRUE
+#define AVRC_INCLUDED                 TRUE
+#define PAN_NAP_DISABLED              TRUE
+
+#define BTA_DM_PM_SNIFF_MAX      2048
+#define BTA_DM_PM_SNIFF_MIN      1024
+#define BTA_DM_PM_SNIFF_ATTEMPT  4
+#define BTA_DM_PM_SNIFF_TIMEOUT  1
+
+#define GAP_TRANSPORT_SUPPORTED       GATT_TRANSPORT_LE
+#define GATTP_TRANSPORT_SUPPORTED     GATT_TRANSPORT_LE
+
+#define BTM_DEFAULT_SCAN_TYPE         BTM_SCAN_TYPE_STANDARD
+
+
+#define AVRC_TG_FEATURES (BTRC_FEAT_METADATA | BTRC_FEAT_ABSOLUTE_VOLUME)
+
+#define AVRC_TG_EVENTS (BTRC_EVT_PLAY_STATUS_CHANGED | \
+                        BTRC_EVT_TRACK_CHANGE | BTRC_EVT_AVAL_PLAYER_CHANGE | \
+                        BTRC_EVT_ADDR_PLAYER_CHANGE | BTRC_EVT_UIDS_CHANGED)
 
 /* HFP HF CLIENT features */
 #define BTHF_CLIENT_FEAT_ECNR  0x00000001 /* Echo cancellation and/or noise reduction */
@@ -49,12 +78,6 @@
     (BT_AG_FEAT_ECNR | BT_AG_FEAT_REJECT |  \
      BT_AG_FEAT_ECS | BT_AG_FEAT_EXTERR |   \
      BT_AG_FEAT_HF_IND | BT_AG_FEAT_ESCO |  \
-                            BT_AG_FEAT_UNAT | BT_AG_FEAT_CODEC)
-
-#define AVRC_TG_FEATURES (BTRC_FEAT_METADATA | BTRC_FEAT_ABSOLUTE_VOLUME)
-
-#define AVRC_TG_EVENTS (BTRC_EVT_PLAY_STATUS_CHANGED | \
-                        BTRC_EVT_TRACK_CHANGE | BTRC_EVT_AVAL_PLAYER_CHANGE | \
-                        BTRC_EVT_ADDR_PLAYER_CHANGE | BTRC_EVT_UIDS_CHANGED)
+     BT_AG_FEAT_UNAT | BT_AG_FEAT_CODEC)
 
 #endif
