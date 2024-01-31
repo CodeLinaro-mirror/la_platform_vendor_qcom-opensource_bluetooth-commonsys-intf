13b864102dd0f2346f7e073dc2c55d4bad657bd9
Janaki Ramaiah Thota <quic_janathot@quicinc.com>
2024-01-31 18:43:26 +0530

    BT: remove BTProfileTestApp from debug build
    Change-Id: I0dcd5c5eec114b455181e9a02d3a6142916e2c93

--- bt-system-opensource-product.mk
+++ bt-system-opensource-product.mk
@@ -71,7 +71,6 @@ PRODUCT_PACKAGES_DEBUG += l2test_ertm
 PRODUCT_PACKAGES_DEBUG += rfc
 
 PRODUCT_PACKAGES_DEBUG += wearos_bluetooth_rfcomm_testapp
-PRODUCT_PACKAGES_DEBUG += BTProfileTestApp
 
 ifneq ($(TARGET_HAS_LOW_RAM), true)
 PRODUCT_PACKAGES_DEBUG += BTTestApp
