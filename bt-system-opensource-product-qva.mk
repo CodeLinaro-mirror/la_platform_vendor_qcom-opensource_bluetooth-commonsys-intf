28bd25d4e45ec1df272f1406e169ae5f7af167ed
Fei Zheng <quic_zfei@quicinc.com>
2024-11-19 18:09:38 +0800

    Bluetooth: Disable GATT over BR/EDR
    Set the property "bluetooth.gatt.over_bredr.enabled" as
    false to disable GATT over BR/EDR
    
    Change-Id: I7827a6d518344bd993c5d92fffcd4df5e83fdcc6

--- bt-system-opensource-product-qva.mk
+++ bt-system-opensource-product-qva.mk
@@ -3,3 +3,5 @@
 # Set max connected audio devices as 1
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.bluetooth.maxconnectedaudiodevices=1
+PRODUCT_PRODUCT_PROPERTIES += \
+    bluetooth.gatt.over_bredr.enabled=false
