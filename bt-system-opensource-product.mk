79388e404feb7be0c16a98a66aa202be7bd87194
Kuichu Ni <kuichun@qti.qualcomm.com>
2025-10-14 12:37:13 +0800

    Refine Bluetooth makefile
    1. Use original setting as the base configuration and disable/enable
       profiles for car role.
    
    2. Disable useless packages, such as Xpan and BtAV.
    
    Change-Id: Ie423e1635d3e83468ffebfa1b38e5f3e52a17adc

--- bt-system-opensource-product.mk
+++ bt-system-opensource-product.mk
@@ -1,98 +1,56 @@
-#ANT
-ifeq ($(TARGET_FWK_SUPPORTS_FULL_VALUEADDS), true)
-ifeq ($(BOARD_ANT_WIRELESS_DEVICE), "vfs-prerelease")
-PRODUCT_PACKAGES += AntHalService
-PRODUCT_PACKAGES += libantradio
-PRODUCT_PACKAGES += antradio_app
-else
-PRODUCT_PACKAGES += AntHalService-Soong
-ifneq ($(TARGET_BOARD_DERIVATIVE_SUFFIX),$(filter $(TARGET_BOARD_DERIVATIVE_SUFFIX), _sdvcomm _sdv _cdccomm _cdcsdv))
-PRODUCT_PACKAGES += com.dsi.ant@1.0
-endif
-endif
-endif #TARGET_FWK_SUPPORTS_FULL_VALUEADDS
-
 #BT
 ifeq ($(BOARD_HAVE_BLUETOOTH_QCOM),true)
-PRODUCT_PACKAGES += Bluetooth
+# BT Related Test app & Tools
+PRODUCT_PACKAGES_DEBUG += gatt_tool_qti_internal
+PRODUCT_PACKAGES_DEBUG += l2test_ertm
 
 ifneq ($(TARGET_BOARD_TYPE),auto)
 # Set supported Bluetooth profiles to enabled
 PRODUCT_PRODUCT_PROPERTIES += \
-    bluetooth.profile.a2dp.source.enabled=true \
-    bluetooth.profile.avrcp.target.enabled=true \
+    bluetooth.profile.a2dp.source.enabled=false \
+    bluetooth.profile.avrcp.target.enabled=false \
     bluetooth.profile.avrcp.controller.enabled=true \
-    bluetooth.profile.hfp.ag.enabled=true \
-    bluetooth.profile.asha.central.enabled=true \
+    bluetooth.profile.hfp.ag.enabled=false \
     bluetooth.profile.gatt.enabled=true \
     bluetooth.profile.hid.host.enabled=true \
-    bluetooth.profile.hid.device.enabled=true \
-    bluetooth.profile.map.server.enabled=true \
+    bluetooth.profile.hid.device.enabled=false \
+    bluetooth.profile.map.server.enabled=false \
     bluetooth.profile.opp.enabled=true \
+    bluetooth.profile.pbap.server.enabled=false \
+    bluetooth.profile.bas.client.enabled=false \
+    bluetooth.profile.a2dp.sink.enabled=true \
+    bluetooth.profile.hfp.hf.enabled=true \
+    bluetooth.profile.map.client.enabled=true \
+    bluetooth.profile.pbap.client.enabled=true \
+    bluetooth.device_id.vendor_id=0x001D \
+ifneq ($(TARGET_SUPPORTS_WEAR_ANDROID), true)
     bluetooth.profile.pan.nap.enabled=true \
     bluetooth.profile.pan.panu.enabled=true \
-    bluetooth.profile.pbap.server.enabled=true \
-    bluetooth.profile.sap.server.enabled=true
-endif #TARGET_BOARD_TYPE
+endif #TARGET_SUPPORTS_WEAR_ANDROID
 
-ifeq ($(TARGET_FWK_SUPPORTS_FULL_VALUEADDS), true)
-ifeq ($(TARGET_BOARD_TYPE),auto)
-TARGET_USE_AUTO_BT_STACK := true
-else
-TARGET_USE_QTI_BT_STACK := true
-endif
-
-ifeq ($(TARGET_USE_QTI_BT_STACK),true)
-# BT Related Libs
-PRODUCT_PACKAGES += libbluetooth_qti
-PRODUCT_PACKAGES += libbluetooth_qti_jni
-PRODUCT_PACKAGES += bt_logger
-PRODUCT_PACKAGES += libbt-logClient
-PRODUCT_PACKAGES += libbtconfigstore
-PRODUCT_PACKAGES += vendor.qti.hardware.btconfigstore@1.0
-PRODUCT_PACKAGES += vendor.qti.hardware.btconfigstore@2.0
-PRODUCT_PACKAGES += com.qualcomm.qti.bluetooth_audio@1.0
-PRODUCT_PACKAGES += vendor.qti.hardware.bluetooth_audio@2.0
-
-ifeq ($(TARGET_USE_BT_DUN),true)
-PRODUCT_PACKAGES += vendor.qti.hardware.bluetooth_dun-V1.0-java
-PRODUCT_PACKAGES += BluetoothExt
-endif #TARGET_USE_BT_DUN
-
-PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/commonsys/packages/apps/Bluetooth
-PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/commonsys/system/bt/conf
-PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/commonsys/system/bt/main
-
-PRODUCT_PACKAGE_OVERLAYS += vendor/qcom/opensource/commonsys-intf/bluetooth/overlay/qva
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := vendor/qcom/opensource/commonsys-intf/bluetooth/build/qva/config
-
-# BT Related Test app & Tools
-PRODUCT_PACKAGES_DEBUG += btsnoop
-PRODUCT_PACKAGES_DEBUG += gatt_tool_qti_internal
-PRODUCT_PACKAGES_DEBUG += l2cap_coc_tool
-PRODUCT_PACKAGES_DEBUG += l2test_ertm
-PRODUCT_PACKAGES_DEBUG += rfc
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    bluetooth.profile.sap.server.enabled=false \
+    bluetooth.profile.pbap.sim.enabled=false \
+    bluetooth.hfp.codec_aptx_voice.enabled=true \
+    bluetooth.hfp.swb.aptx.power_management.enabled=true \
+    ro.bluetooth.leaudio_offload.supported=true \
+    bluetooth.device.class_of_device=26,4,8 \
+    persist.bluetooth.leaudio_offload.disabled=false \
+    persist.bluetooth.leaudio.bypass_allow_list=true \
+    bluetooth.leaudio.dual_bidirection_swb.supported=true \
+    persist.bluetooth.leaudio.notify.idle.during.call=true \
+    persist.vendor.bluetooth.haltest=true \
+    bluetooth.core.le.max_number_of_concurrent_connections=10
 
 ifneq ($(TARGET_HAS_LOW_RAM), true)
-PRODUCT_PACKAGES_DEBUG += BTTestApp
+#PRODUCT_PACKAGES += Xpan
+#PRODUCT_PACKAGES += privapp-permission-xpan.xml
+#PRODUCT_PACKAGES += ChannelSoundingTestApp
+#PRODUCT_PACKAGES += com.android.bluetooth.channelsoundingtestapp.xml
+#PRODUCT_PACKAGES += BtVS
 endif #TARGET_HAS_LOW_RAM
 
-else
-#PRODUCT_SOONG_NAMESPACES += packages/modules/Bluetooth/android/app
-#PRODUCT_PACKAGE_OVERLAYS += vendor/qcom/opensource/commonsys-intf/bluetooth/overlay/generic
-#BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := vendor/qcom/opensource/commonsys-intf/bluetooth/build/generic/config
-endif #TARGET_USE_QTI_BT_STACK
-
-else
-#PRODUCT_PACKAGE_OVERLAYS += vendor/qcom/opensource/commonsys-intf/bluetooth/overlay/generic
-#BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := vendor/qcom/opensource/commonsys-intf/bluetooth/build/generic/config
-endif #TARGET_FWK_SUPPORTS_FULL_VALUEADDS
-
-ifeq ($(TARGET_USE_AUTO_BT_STACK),true)
-include vendor/qcom/opensource/commonsys-intf/bluetooth/bt-system-opensource-product-qva.mk
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := vendor/qcom/opensource/commonsys-intf/bluetooth/build/auto/config
-endif #TARGET_USE_AUTO_BT_STACK
-
+endif #TARGET_BOARD_TYPE
 endif #BOARD_HAVE_BLUETOOTH_QCOM
 
 #FM
@@ -110,18 +68,3 @@ PRODUCT_PACKAGES += vendor.qti.hardware.fm@1.0
 PRODUCT_PROPERTY_OVERRIDES += vendor.hw.fm.init=0
 endif #TARGET_FWK_SUPPORTS_FULL_VALUEADDS
 endif #BOARD_HAVE_QCOM_FM
-
-#WIPOWER
-ifeq ($(BOARD_USES_WIPOWER),true)
-ifeq ($(TARGET_FWK_SUPPORTS_FULL_VALUEADDS), true)
-#WIPOWER, wbc
-PRODUCT_PACKAGES += wbc_hal.default
-PRODUCT_PACKAGES += com.quicinc.wbc
-PRODUCT_PACKAGES += com.quicinc.wbc.xml
-PRODUCT_PACKAGES += com.quicinc.wbcservice
-PRODUCT_PACKAGES += com.quicinc.wbcservice.xml
-PRODUCT_PACKAGES += libwbc_jni
-PRODUCT_PACKAGES += com.quicinc.wipoweragent
-PRODUCT_PACKAGES += com.quicinc.wbcserviceapp
-endif #TARGET_FWK_SUPPORTS_FULL_VALUEADDS
-endif #BOARD_USES_WIPOWER
